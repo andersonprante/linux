e003ca9b7ca4ecc52827ebc6488752f62b494ac2
Anderson Prante <andersonprante@gmail.com>
2018-12-06 13:40:15 -0200

    fix remove_old_temp

--- scripts/remove_old_temp.sh
+++ scripts/remove_old_temp.sh
@@ -3,6 +3,6 @@
 # CRONTAB
 # 0 */6 * * * $PATH/remove_old_temp.sh
 
-find ~/temp/ -mtime +15 -exec rm {} \;
+find ~/temp/ -mtime +15 -exec rm -r {} \;
 
 echo "$(date +%Y\/%m\/%d\ %H:%M:%S) Limpou pasta ~/temp/" >> /tmp/limpezaTEMP.log
